eba8ae834a9c70b1b03dd87e22f5212109088dc0
foevn <foevn@DESKTOP-RIQS2IL>
2020-07-06 21:57:36 +0900

    getMaxDaysOfMonth Method update

--- src/young/calendar/Calendar.java
+++ src/young/calendar/Calendar.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class Calendar {
 
+	private static final int[] MaxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getMaxDaysOfMonth(int month) {
+		return MaxDays[month-1];
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("일    월   화   수   목   금   토");
 		System.out.println("--------------------");
@@ -14,13 +20,16 @@ public class Calendar {
 
 		
 		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		
 		System.out.println("월을 입력하세요.");
 		int month = scanner.nextInt();
-		int[] MaxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		
-		System.out.printf("%d월은 %d일 까지 입니다.",month,MaxDays[month-1]);
+		System.out.printf("%d월은 %d일 까지 입니다.",month,cal.getMaxDaysOfMonth(month));
 	
+		
+		scanner.close();
+		
 		
 		
 		
