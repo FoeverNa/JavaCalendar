1bdcf83ce8011f4720be430f2a22cfcc8dc20643
foevn <foevn@DESKTOP-RIQS2IL>
2020-07-06 21:59:28 +0900

    printSampleCalendar Update

--- src/young/calendar/Calendar.java
+++ src/young/calendar/Calendar.java
@@ -9,15 +9,16 @@ public class Calendar {
 	public int getMaxDaysOfMonth(int month) {
 		return MaxDays[month-1];
 	}
-	
-	public static void main(String[] args) {
+	public void printSampleCalendar () {
 		System.out.println("일    월   화   수   목   금   토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-
+	}
+	
+	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
@@ -25,8 +26,9 @@ public class Calendar {
 		System.out.println("월을 입력하세요.");
 		int month = scanner.nextInt();
 		
-		System.out.printf("%d월은 %d일 까지 입니다.",month,cal.getMaxDaysOfMonth(month));
-	
+		System.out.printf("%d월은 %d일 까지 입니다.\n",month,cal.getMaxDaysOfMonth(month));
+		
+		cal.printSampleCalendar();
 		
 		scanner.close();
 		
