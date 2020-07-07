055709a5634fbd60e8caa6066bb02c3b9db46731
foevn <foevn@DESKTOP-RIQS2IL>
2020-07-07 13:33:13 +0900

    update loop

--- src/young/calendar/Calendar.java
+++ src/young/calendar/Calendar.java
@@ -23,13 +23,21 @@ public class Calendar {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		
+		System.out.println("반복횟수를 입력하세요");
+		int repeat = scanner.nextInt();
+		
+		
+		
+		for (int i = 0; i < repeat; i++) {
+			
 		System.out.println("월을 입력하세요.");
 		int month = scanner.nextInt();
-		
 		System.out.printf("%d월은 %d일 까지 입니다.\n",month,cal.getMaxDaysOfMonth(month));
+				
+		System.out.println("");
+		}
 		
-		cal.printSampleCalendar();
-		
+		System.out.println("bye~");
 		scanner.close();
 		
 		
