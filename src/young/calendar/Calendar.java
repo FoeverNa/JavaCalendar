079714e62e2f85bd464f95aa8a51335a8e28420c
foevn <foevn@DESKTOP-RIQS2IL>
2020-07-07 15:49:30 +0900

    My version of Leap calendar updated

--- src/young/calendar/Calendar.java
+++ src/young/calendar/Calendar.java
@@ -11,24 +11,49 @@ public class Calendar {
 	}
 
 	public static void printCalendar(int year, int month) {
-
 		System.out.printf("   <<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TU FR SA");
 		System.out.println("----------------------");
 		int maxDay = getMaxDaysOfMonth(month);
 
-		for (int i = 1; i <= maxDay; i++) {
-			System.out.printf("%3d", i);
-			if (i % 7 == 0) {
-				System.out.println();
+		if (month == 2) {
+			if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+
+				for (int i = 1; i <= maxDay+1; i++) {
+					System.out.printf("%3d", i);
+					if (i % 7 == 0) {
+						System.out.println();
+					}
+				}
+				
+			} else {
+				
+				for (int i = 1; i <= maxDay; i++) {
+					System.out.printf("%3d", i);
+					if (i % 7 == 0) {
+						System.out.println();
+					}
+				}		
 			}
 
+		} else {
+			
+			for (int i = 1; i <= maxDay; i++) {
+				System.out.printf("%3d", i);
+				if (i % 7 == 0) {
+					System.out.println();
+				}
+			}		
 		}
-		System.out.println();
+		
+				
+				
 //		System.out.println(" 1  2  3  4  5  6  7");
 //		System.out.println(" 8  9 10 11 12 13 14");
 //		System.out.println("15 16 17 18 19 20 21");
 //		System.out.println("22 23 24 25 26 27 28");
-	}
+			
 
+		
+	}
 }
